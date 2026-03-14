c0a9b89ac2cd05713d61513c9aa425ba955ac2de
Anitsa Kalinova <anitsa011@gmail.com>
2026-03-14 21:39:36 +0100

    checking git repo - finish

--- 02-JS-Asynchronous-Programming-Lab/02.Github-Repos/scripts/app.js
+++ 02-JS-Asynchronous-Programming-Lab/02.Github-Repos/scripts/app.js
@@ -1,3 +1,35 @@
 function loadRepos() {
-	console.log("TODO...");
+	const list = document.getElementById('repos');
+	const userName = document.getElementById('username').value;
+	const url = `https://api.github.com/users/${userName}/repos`;
+
+	fetch(url)
+	    .then(handleResponse)
+		.then(handleData)
+		.catch(handleError);
+
+	function handleResponse(response) {
+		if (response.ok) {
+			return response.json();
+		} else {
+			throw response.json();
+		}
+	}
+
+	function handleData(data) {
+		list.replaceChildren();
+
+		for (const repo of data) {
+			list.innerHTML += 
+			`<li>
+            <a href="${repo.html_url}">
+                ${repo.full_name}
+            </a>
+        </li>`
+		}
+	}
+
+	function handleError(errorPromise) {
+		errorPromise.then(error => {list.innerHTML = error.message})
+	}
 }
