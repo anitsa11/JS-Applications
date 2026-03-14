4eed598b7767c46f7bd003df9f3454a947585caa
Anitsa Kalinova <anitsa011@gmail.com>
2026-03-14 22:38:39 +0100

    exercise_3 - github commits- f

--- 02-JS-Asynchronous-Programming-Lab/03.Gighub-Commits/app.js
+++ 02-JS-Asynchronous-Programming-Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,25 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+    const username = document.getElementById('username').value;
+    const repo = document.getElementById('repo').value;
+    const list = document.getElementById('commits');
+
+    const url = `https://api.github.com/repos/${username}/${repo}/commits`;
+
+    try {
+        const res = await fetch(url);
+
+        if(!res.ok) {
+            throw await res.json();
+        }
+
+        const data = await res.json();
+
+        list.replaceChildren();
+        for (const commit of data) {
+            list.innerHTML += `<li>${commit.commit.author.name}: ${commit.commit.message}</li>`;
+        }
+
+    } catch (error) {
+        list.innerHTML = error.message;
+    }
 }
