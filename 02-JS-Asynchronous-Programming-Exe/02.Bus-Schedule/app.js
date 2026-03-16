cf07355b274471d9aed4f80714f0f8397845d064
Anitsa Kalinova <anitsa011@gmail.com>
2026-03-16 16:44:34 +0100

    bus dhedule exe - f

--- 02-JS-Asynchronous-Programming-Exe/02.Bus-Schedule/app.js
+++ 02-JS-Asynchronous-Programming-Exe/02.Bus-Schedule/app.js
@@ -1,11 +1,41 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    const infoBoxRef = document.getElementsByClassName('info')[0];
+    const departBtnRef = document.getElementById('depart');
+    const arriveBtnRef = document.getElementById('arrive');
+
+    const stop = {
+        currentStop: '',
+        nextStop: "depot"
+    }
+
+    async function depart() {
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${stop}`
+
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+
+            stop.nextStop = data.next;
+            stop.currentStop = data.name;
+
+            infoBoxRef.textContent = `Next stop ${data.name}`;
+            console.log(data);
+            departBtnRef.disabled = true;
+            arriveBtnRef.disabled = false;
+
+        } catch (error) {
+            infoBoxRef.textContent = 'Error';
+            departBtnRef.disabled = true;
+            arriveBtnRef.disabled = true;
+        }
+
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoBoxRef.textContent = `Arriving at ${stop.currentStop}`;
+        departBtnRef.disabled = false;
+        arriveBtnRef.disabled = true;
     }
 
     return {
