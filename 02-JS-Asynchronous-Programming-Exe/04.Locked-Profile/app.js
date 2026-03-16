70e3111a752030e8b79b970e641df3a4922f69dd
Anitsa Kalinova <anitsa011@gmail.com>
2026-03-16 20:46:39 +0100

    locked profile -async-f

--- 02-JS-Asynchronous-Programming-Exe/04.Locked-Profile/app.js
+++ 02-JS-Asynchronous-Programming-Exe/04.Locked-Profile/app.js
@@ -1,3 +1,73 @@
-function lockedProfile() {
-    console.log('TODO...')
+async function lockedProfile() {
+
+    const url = 'http://localhost:3030/jsonstore/advanced/profiles';
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    Object.values(data).forEach(profile => {
+        createProfile(profile);
+    });
+
+    function createProfile(user) {
+
+        const main = document.getElementById('main');
+
+        const profileDiv = document.createElement('div');
+        profileDiv.className = 'profile';
+
+        profileDiv.innerHTML = `
+            <img src="./iconProfile2.png" class="userIcon">
+
+            <label>Lock</label>
+            <input type="radio" name="user${user._id}Locked" value="lock" checked>
+
+            <label>Unlock</label>
+            <input type="radio" name="user${user._id}Locked" value="unlock">
+
+            <br>
+            <hr>
+
+            <label>Username</label>
+            <input type="text" value="${user.username}" disabled readonly>
+
+            <div class="hiddenInfo" style="display:none">
+                <hr>
+
+                <label>Email:</label>
+                <input type="email" value="${user.email}" disabled readonly>
+
+                <label>Age:</label>
+                <input type="text" value="${user.age}" disabled readonly>
+            </div>
+
+            <button>Show more</button>
+        `;
+
+        const button = profileDiv.querySelector('button');
+        button.addEventListener('click', toggleInfo);
+
+        main.appendChild(profileDiv);
+    }
+
+    function toggleInfo(e) {
+
+        const profile = e.target.parentElement;
+
+        const unlockRadio = profile.querySelector('input[value="unlock"]');
+        const hiddenDiv = profile.querySelector('.hiddenInfo');
+        const button = profile.querySelector('button');
+
+        if (!unlockRadio.checked) {
+            return;
+        }
+
+        if (button.textContent === 'Show more') {
+            hiddenDiv.style.display = 'block';
+            button.textContent = 'Hide it';
+        } else {
+            hiddenDiv.style.display = 'none';
+            button.textContent = 'Show more';
+        }
+    }
 }
