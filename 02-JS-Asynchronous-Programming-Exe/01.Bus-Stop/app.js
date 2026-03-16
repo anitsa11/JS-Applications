7cb7cbd0aeed1dc55c6e1ce91717e9339de3d785
Anitsa Kalinova <anitsa011@gmail.com>
2026-03-16 15:38:55 +0100

    busStop exe - f

--- 02-JS-Asynchronous-Programming-Exe/01.Bus-Stop/app.js
+++ 02-JS-Asynchronous-Programming-Exe/01.Bus-Stop/app.js
@@ -1,3 +1,30 @@
 function getInfo() {
-    console.log("TODO...");
+    const base_url = "http://localhost:3030/jsonstore/bus/businfo/";
+    const inputRef = document.getElementById('stopId');
+    const stopNameRef = document.getElementById('stopName');
+    const busInfoRef = document.getElementById('buses');
+
+    const stopId = inputRef.value;
+    if (!stopId) {
+        return;
+    }
+
+    inputRef.value = '';
+    busInfoRef.textContent = '';
+
+    const response = fetch(base_url + stopId);
+
+    response.then(res => {
+        res.json()
+            .then(data => {
+                stopNameRef.textContent = data.name;
+                Object.entries(data.buses).forEach(bus => {
+                    const li = document.createElement('li');
+                    li.textContent = `Bus ${bus[0]} arrives in ${bus[1]} minutes`;
+                    busInfoRef.appendChild(li);
+                })
+            }).catch(err => {stopNameRef.textContent = 'Error'})
+    }).catch(err => {stopNameRef.textContent = 'Error'})
+
+    
 }
