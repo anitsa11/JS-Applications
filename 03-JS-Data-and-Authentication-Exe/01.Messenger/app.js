f5a7058d96e0a38e2bcbac609c7df6c0597933ee
Anitsa Kalinova <anitsa011@gmail.com>
2026-03-21 19:42:25 +0100

    messenger exe-f / onSubmit and onLoad btn

--- 03-JS-Data-and-Authentication-Exe/01.Messenger/app.js
+++ 03-JS-Data-and-Authentication-Exe/01.Messenger/app.js
@@ -1,5 +1,64 @@
 function attachEvents() {
-    console.log('TODO...');
+   const URL ='http://localhost:3030/jsonstore/messenger';
+
+   const textAreaRef = document.getElementById('messages');
+
+   document.getElementById('submit').addEventListener('click', onSubmit);
+   document.getElementById('refresh').addEventListener('click', onLoad);
+
+   async function onLoad(params) {
+    const response = await fetch(URL);
+    if(response.status !== 200) {
+        return;
+    }
+
+    const data = await response.json();
+
+    let buff = "";
+
+    Object.values(data).forEach( rec => {
+        buff += `${rec.author}: ${rec.content}\n`;
+    })
+
+    buff.trim();
+    textAreaRef.value = buff;
+    
+   }
+
+   async function onSubmit(e) {
+    const authorRef = document.querySelector("input[name='author']");
+    const contentRef = document.querySelector("input[name='content']");
+
+
+
+    const author = authorRef.value;
+    const content = contentRef.value;
+
+    if (!author || !content) {
+        return;
+    }
+
+    const data = {
+        author,
+        content
+    };
+
+    const option = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+
+    await fetch(URL, option);
+
+    authorRef.value ='';
+    contentRef.value ='';
+
+    onLoad();
+   }
 }
+
 
 attachEvents();
