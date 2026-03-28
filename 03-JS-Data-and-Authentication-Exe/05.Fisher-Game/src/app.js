4e3da273959f6ed49a155b88cb733ecec7ddcb60
Anitsa Kalinova <anitsa011@gmail.com>
2026-03-28 21:50:50 +0100

    load menu -f

--- 03-JS-Data-and-Authentication-Exe/05.Fisher-Game/src/app.js
+++ 03-JS-Data-and-Authentication-Exe/05.Fisher-Game/src/app.js
@@ -1,3 +1,4 @@
+
 function app() {
     const endPoints = {
         logout: "http://localhost:3030/users/logout",
@@ -12,9 +13,15 @@ function app() {
 
     const userNameRef = document.querySelector("nav span");
 
+    const catchesRef = document.getElementById("catches");
+    const form = document.querySelector("form");
+    const addBtn = form.querySelector("button");
+
+    onLoadAllCatches();
+
     updateNav();
 
-    async function onLogout () {
+    async function onLogout() {
 
         const option = {
             method: "GET",
@@ -36,13 +43,56 @@ function app() {
             userNavSec.style.display = "inline-block";
             guestNavSec.style.display = "none";
             userNameRef.textContent = userData.email;
+
+            addBtn.disabled = false;
         } else {
             userNavSec.style.display = "none";
             guestNavSec.style.display = "inline-block";
             userNameRef.textContent = "guest";
 
+            addBtn.disabled = true;
+
         }
 
+    }
+
+    async function onLoadAllCatches() {
+        const response = await fetch(endPoints.catches);
+        const data = await response.json();
+
+        catchesRef.innerHTML = "";
+
+        showAllCatches(data);
+
+    }
+
+    function showAllCatches(data) {
+        data.forEach(el => {
+            const div = document.createElement("div")
+            div.classList.add("catch")
+            const content = createContent(el);
+            div.innerHTML = content;
+            catchesRef.appendChild(div);
+        })
+    }
+
+    function createContent(el) {
+        return `
+         <label>Angler</label>
+          input type="text" class="angler" value=${el.angler}>
+            <label>Weight</label>
+            <input type="text" class="weight" value=${el.weight}>
+            <label>Species</label>
+             <input type="text" class="species" value=${el.species}>
+            <label>Location</label>
+            <input type="text" class="location" value=${el.location}>
+             <label>Bait</label>
+            <input type="text" class="bait" value=${el.bait}>
+            <label>Capture Time</label>
+            <input type="number" class="captureTime" value=${el.captureTime}>
+            <button class="update" data-id=${el._ownerId} ${el._ownerId !== userData?._id ? "disabled": ""}>Update</button>
+            <button class="delete" data-id=${el._ownerId} ${el._ownerId !== userData?._id ? "disabled": ""}>Delete</button>
+        `
     }
 }
 
